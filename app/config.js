2d94ba481f4c005739434f1562aad5e9458238c6
PranavKumar20 <pranavkumar0155@gmail.com>
2024-09-26 21:29:29 +0530

    changed API_URL

--- app/config.js
+++ app/config.js
@@ -1,2 +1,3 @@
 // src/config.js (or config.ts)
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://totality-frontend-challenge-sigma.vercel.app/api';
+// export const API_URL = 'http://localhost:3000/api';
